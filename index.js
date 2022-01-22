8ae83b5f42e55309c55f92868f429c2a22c71a9a
Miyakawa Takuya <omyakawa@gmail.com>
2022-01-23 06:55:00 +0900

    add index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+//https://qiita.com/kibinag0/items/f1a9021cb0383c4771d5 こちらのファイルを参照
+//jshint esversion:6
+
+//expressをロードする
+const express = require("express");
+const app = express();
+
+
+// public内ファイルを使用できるようにする
+app.use(express.static("public"));
+
+
+//requestがあった時のresponse処理
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+
+});
+
+// process.env.PORTはdynamic portとしての機能
+app.listen(process.env.PORT || 3000, function () {
+    console.log("The server is runnning on port 3000.");
+});
