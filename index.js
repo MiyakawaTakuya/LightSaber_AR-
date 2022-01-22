4617a23781cf153b7c9291815380531ff38e9474
Miyakawa Takuya <omyakawa@gmail.com>
2022-01-23 07:34:02 +0900

    faviconをコメントアウト

--- index.js
+++ index.js
@@ -9,15 +9,14 @@ const app = express();
 // public内ファイルを使用できるようにする
 app.use(express.static("public"));
 
-
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));  うまくいかないのでコメントアウト
 //requestがあった時のresponse処理
 app.get("/", function (req, res) {
     // res.sendFile(__dirname + "/index.html");
     res.sendFile(__dirname + "/public/index.html");
 
 });
-app.use(express.static('public'));
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 // process.env.PORTはdynamic portとしての機能
 app.listen(process.env.PORT || 3000, function () {
     console.log("The server is runnning on port 3000.");
